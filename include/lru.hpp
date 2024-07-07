35f3dc8eaf9cad6d814cc2126b541a1daf49fe56
Cristian Bosin <cristian.maruanbosin@wexinc.com>
2024-07-07 16:04:07 -0300

    Documentation

--- include/lru.hpp
+++ include/lru.hpp
@@ -9,17 +9,40 @@
 #include <map>
 #include <memory>
 
+/**
+ * @file lru.hpp
+ * @brief Defines a Least Recently Used (LRU) cache.
+ */
 
+/**
+ * @class lru
+ * @tparam Key The type of the keys in the cache.
+ * @tparam Value The type of the values in the cache.
+ * @brief A Least Recently Used (LRU) cache.
+ *
+ * This class implements a Least Recently Used (LRU) cache. The cache has a fixed size,
+ * specified at construction time. When the cache is full and a new item is inserted,
+ * the least recently used item is removed from the cache.
+ */
 template<class Key, class Value>
 class lru {
 public:
-    using key_type = Key;
-    using value_type = Value;
-    using pointer = std::shared_ptr<value_type>;
-    using weak_pointer = std::weak_ptr<value_type>;
+    using key_type = Key; ///< The type of the keys in the cache.
+    using value_type = Value; ///< The type of the values in the cache.
+    using pointer = std::shared_ptr<value_type>; ///< A shared pointer to a value.
+    using weak_pointer = std::weak_ptr<value_type>; ///< A weak pointer to a value.
 
+    /**
+     * @brief Constructs a new LRU cache.
+     * @param size The size of the cache.
+     */
     explicit lru(size_t size) : m_size(size) {}
 
+    /**
+     * @brief Inserts a new item into the cache.
+     * @param key The key of the item.
+     * @param ptr A shared pointer to the item.
+     */
     void insert(key_type key, pointer ptr) {
         if (m_list.size() >= m_size) {
             m_list.pop_back();
@@ -29,14 +52,28 @@ public:
         m_iter_map[key] = m_list.begin();
     }
 
+    /**
+     * @brief Inserts a new item into the cache.
+     * @param key The key of the item.
+     * @param value The value of the item.
+     */
     void insert(key_type key, value_type &&value) {
         insert(key, std::make_shared<value_type>(std::move(value)));
     }
 
+    /**
+     * @brief Inserts a new item into the cache.
+     * @param pair The key-value pair of the item.
+     */
     void insert(std::pair<key_type, value_type> &&pair) {
         insert(pair.first, std::move(pair.second));
     }
 
+    /**
+     * @brief Retrieves an item from the cache.
+     * @param key The key of the item.
+     * @return A shared pointer to the item if it exists in the cache, otherwise nullptr.
+     */
     pointer get(key_type key) {
         auto it = m_map.find(key);
         if (it == m_map.end()) {
@@ -57,10 +94,10 @@ public:
 
 private:
     using list_iterator = typename std::list<pointer>::iterator;
-    std::list<pointer> m_list;
-    std::map<key_type, weak_pointer> m_map;
-    std::map<key_type, list_iterator> m_iter_map;
-    size_t m_size;
+    std::list<pointer> m_list; ///< A list of pointers to the items in the cache.
+    std::map<key_type, weak_pointer> m_map; ///< A map from keys to weak pointers to the items in the cache.
+    std::map<key_type, list_iterator> m_iter_map; ///< A map from keys to iterators to the items in the cache.
+    size_t m_size; ///< The size of the cache.
 };
 
 
--- include/page.hpp
+++ include/page.hpp
@@ -11,12 +11,23 @@
 #include <span>
 #include <sstream>
 
+/**
+ * @brief A concept that checks if a type can be streamed to an output stream.
+ */
 template<class T>
 concept Streamable = requires(std::ostream &os, T a) {
     os << a;
 };
 
-
+/**
+ * @class page
+ * @tparam PageSize The size of the page.
+ * @brief A class that represents a page in a database.
+ *
+ * This class represents a page in a database. It provides methods for inserting data into the page,
+ * retrieving data from the page, and checking if the page contains a given index. It also provides
+ * methods for checking if the page is empty, if it fits a given size, and for clearing the page.
+ */
 template<size_t PageSize = 16384>
 class page {
 public:
@@ -30,6 +41,10 @@ public:
     using intra_offset_t = std::conditional_t<page_size <= 16384, uint16_t, uint32_t>;
     using inter_offset_t = uint32_t;
 
+    /**
+     * @struct header
+     * @brief A struct that represents the header of a page.
+     */
     struct header {
         intra_offset_t free_region_start;
         intra_offset_t free_region_end;
@@ -37,19 +52,37 @@ public:
 
     constexpr static size_t data_size = PageSize - sizeof(header);
 
+    /**
+     * @brief Constructs a new page.
+     */
     page() {
         m_header.free_region_start = sizeof(header);
         m_header.free_region_end = data_size;
     }
 
+    /**
+     * @brief Checks if the page is empty.
+     * @return true if the page is empty, false otherwise.
+     */
     [[nodiscard]] bool empty() const {
         return size() == 0;
     }
 
+    /**
+     * @brief Checks if the page contains a given index.
+     * @param index The index to check.
+     * @return true if the page contains the index, false otherwise.
+     */
     [[nodiscard]] bool contains(size_t index) const {
         return index < size();
     }
 
+    /**
+     * @brief Retrieves a const view of the data at a given index.
+     * @param index The index of the data.
+     * @return A const view of the data.
+     * @throws std::out_of_range If the index is out of range.
+     */
     [[nodiscard]] const_view at(size_t index) const {
         if (not contains(index)) {
             throw std::out_of_range("Index out of range");
@@ -60,12 +93,23 @@ public:
         return {first, last};
     }
 
+    /**
+     * @brief Retrieves a view of the data at a given index.
+     * @param index The index of the data.
+     * @return A view of the data.
+     */
     view_type at(size_t index) {
         auto view = const_cast<const page *>(this)->at(index);
         return {const_cast<value_type *>(view.data()), view.size()};
     }
 
-
+    /**
+     * @brief Inserts data into the page.
+     * @tparam T The type of the data.
+     * @param data The data to insert.
+     * @return The index of the inserted data.
+     * @throws std::bad_alloc If the data does not fit in the page.
+     */
     template<Streamable T>
     // require non-string type
         requires(not std::same_as<T, std::string> and not std::same_as<T, std::string_view> and not std::same_as<T, const char *>)
@@ -75,6 +119,12 @@ public:
         return insert(os.str());
     }
 
+    /**
+     * @brief Inserts a string view into the page.
+     * @param data The string view to insert.
+     * @return The index of the inserted string view.
+     * @throws std::bad_alloc If the string view does not fit in the page.
+     */
     size_t insert(std::string_view data) {
         if (not fits(data.size() + sizeof(intra_offset_t) + 2)) {
             throw std::bad_alloc();
@@ -91,29 +141,57 @@ public:
         return current_entry;
     }
 
+    /**
+     * @brief Checks if a given size fits in the page.
+     * @param size The size to check.
+     * @return true if the size fits in the page, false otherwise.
+     */
     [[nodiscard]] bool fits(size_t size) const {
         return free_space() >= size;
     }
 
+    /**
+     * @brief Retrieves the size of the page.
+     * @return The size of the page.
+     */
     [[nodiscard]] size_t size() const {
         return (m_header.free_region_start - sizeof(header)) / sizeof(intra_offset_t);
     }
 
+    /**
+     * @brief Clears the page.
+     */
     void clear() {
         m_header.free_region_start = sizeof(header);
         m_header.free_region_end = PageSize;
     }
 
+    /**
+     * @brief Retrieves the free space in the page.
+     * @return The free space in the page.
+     */
     [[nodiscard]] size_t free_space() const {
         return m_header.free_region_end - m_header.free_region_start;
     }
 
+    /**
+     * @brief Writes the page to an output stream.
+     * @param os The output stream.
+     * @param page The page to write.
+     * @return The output stream.
+     */
     friend std::ostream &operator<<(std::ostream &os, const page &page) {
         os.write(reinterpret_cast<const char *>(&page.m_header), sizeof(header));
         os.write(reinterpret_cast<const char *>(page.m_data.data()), page.m_data.size());
         return os;
     }
 
+    /**
+     * @brief Reads a page from an input stream.
+     * @param is The input stream.
+     * @param page The page to read into.
+     * @return The input stream.
+     */
     friend std::istream &operator>>(std::istream &is, page &page) {
         is.read(reinterpret_cast<char *>(&page.m_header), sizeof(header));
         is.read(reinterpret_cast<char *>(page.m_data.data()), page.m_data.size());
@@ -121,13 +199,17 @@ public:
     }
 
 private:
+    /**
+     * @brief Retrieves the size and offset of the data at a given index.
+     * @param index The index of the data.
+     * @return A pair of the size and offset of the data.
+     */
     std::pair<intra_offset_t, const intra_offset_t *> size_and_offset_of(size_t index) const {
         const auto *offsets = reinterpret_cast<const intra_offset_t *>(m_data.data());
         const intra_offset_t data_offset = offsets[index];
         const auto *data_ptr = reinterpret_cast<const intra_offset_t *>(&m_data[data_offset]);
         return {*data_ptr, data_ptr + 1};
     }
-
 
     header m_header;
     std::array<uint8_t, data_size> m_data;
